638bdc68e8dbdd3affac0561b4c10a8bd8da6713
Natasha Matthews <Natasha@MacBook-Pro.local>
2020-03-08 20:26:41 -0500

    added a database with firebase and axios

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,25 +3,25 @@ import React from 'react';
 import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
-const burger = (props) => {
-    let transfromedIngredients = Object.keys(props.ingredients)
-      .map(igKey => {
-        return [...Array(props.ingredients[igKey])].map((_, i) => {
-          return <BurgerIngredient key={igKey + i} type={igKey} />
-        });
-      })
-      .reduce((arr, el) => {
-         return arr.concat(el)
-      }, []);
-      if (transfromedIngredients.length === 0 ) {
-        transfromedIngredients = <p>Pleast start adding ingredients!</p>
-      }
+const burger = ( props ) => {
+    let transformedIngredients = Object.keys( props.ingredients )
+        .map( igKey => {
+            return [...Array( props.ingredients[igKey] )].map( ( _, i ) => {
+                return <BurgerIngredient key={igKey + i} type={igKey} />;
+            } );
+        } )
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients!</p>;
+    }
     return (
-      <div className={classes.Burger}>
-        <BurgerIngredient type="bread-top" />
-        {transfromedIngredients}
-        <BurgerIngredient type="bread-bottom" />
-      </div>
+        <div className={classes.Burger}>
+            <BurgerIngredient type="bread-top" />
+            {transformedIngredients}
+            <BurgerIngredient type="bread-bottom" />
+        </div>
     );
 };
 
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -6,23 +6,23 @@ import classes from './SideDrawer.css';
 import Backdrop from '../../UI/Backdrop/Backdrop';
 import Aux from '../../../hoc/Aux/Aux';
 
-const sideDrawer = (props) => {
+const sideDrawer = ( props ) => {
     let attachedClasses = [classes.SideDrawer, classes.Close];
     if (props.open) {
         attachedClasses = [classes.SideDrawer, classes.Open];
     }
     return (
-      <Aux>
-          <Backdrop show={props.open} clicked={props.closed}/>
-      <div className={attachedClasses.join(' ')}>
-        <div className={classes.Logo}>
-          <Logo />
-        </div>
-        <nav>
-          <NavigationItems />
-        </nav>
-      </div>
-      </Aux>
+        <Aux>
+            <Backdrop show={props.open} clicked={props.closed}/>
+            <div className={attachedClasses.join(' ')}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
+            </div>
+        </Aux>
     );
 };
 
